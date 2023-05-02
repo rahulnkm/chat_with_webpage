c3774902c2d2f04e70c961d1df0dee8ef5d75a6a
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2023-05-02 02:24:59 -0400

    go

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import streamlit as st
+import langchain
+
+from langchain.document_loaders import UnstructuredURLLoader
+
+urls = []
+
+
+loader = UnstructuredURLLoader(urls=urls)
+
+data = loader.load()
+texts = text_splitter.split_text(data)
+
+
+query = "What did the president say about Justice Breyer"
+docs = docsearch.get_relevant_documents(query)
+
+
+chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+query = "What did the president say about Justice Breyer"
+chain.run(input_documents=docs, question=query)
